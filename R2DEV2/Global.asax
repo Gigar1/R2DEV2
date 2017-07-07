1337fbadd543d3b241d801466ddc43eb0087a977
Gigar1 <Gigarkhalil@hotmail.com>
2017-07-07 15:23:51 +0200

    Add project files.

---
+++ R2DEV2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="R2DEV2.MvcApplication" Language="C#" %>
---
+++ R2DEV2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(R2DEV2.Startup))]
+namespace R2DEV2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
