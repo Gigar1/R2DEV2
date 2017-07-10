77d9b21b862a72574f1f1d7876e2b4f9715d5c47
JMetropolis <jcityes@gmail.com>
2017-07-10 09:31:46 +0200

    v0.1 added db stuff

---
+++ R2DEV2/Models/CourseClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace R2DEV2.Models
+{
+    public class CourseClass
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime StartTime { get; set; }
+        public TimeSpan Duration { get; set; }
+        public DateTime EndTime { get { return StartTime + Duration; } }
+        public string Description { get; set; }
+
+        public virtual ICollection<ApplicationUser> AttendingUsers { get; set; }
+
+    }
+}
