cc052d7795199ca114e1e9d8b5ceaff3aac6cd8d
JMetropolis <jcityes@gmail.com>
2017-08-07 13:45:52 +0200

    Added annotations to the models.

--- R2DEV2/Models/Classes/CourseClass.cs
+++ R2DEV2/Models/Classes/CourseClass.cs
@@ -3,17 +3,24 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace R2DEV2.Models.Classes
 {
     public class CourseClass
     {
         public int Id { get; set; }
+        [Display(Name = "Kursnamn")]
         public string Name { get; set; }
+        [Display(Name = "Kursinfo")]
         public string Description { get; set; }
         [DisplayFormat(DataFormatString = "{0:d}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.Date)]
+        [Display(Name = "Startdatum")]
         public DateTime StartTime { get; set; }
         [DisplayFormat(DataFormatString = "{0:d}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.Date)]
+        [Display(Name = "Slutdatum")]
         public DateTime EndTime { get; set; }
 
         public virtual ICollection<ApplicationUser> AttendingStudents { get; set; }
